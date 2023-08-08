4dec3b144214c18ee1430553d21bbd308741e2cb
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2023-08-08 18:57:51 +0900

    modify mainpage

--- 공동/lion/pages/index.tsx
+++ 공동/lion/pages/index.tsx
@@ -4,6 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styled from "@emotion/styled";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 const images = ["/main1.png", "/main2.png", "/main3.png"];
 
@@ -75,7 +76,7 @@ const ItemDescription = styled.p`
   font-size: 14px;
   color: #777;
 `;
-const NoticeLayout= styled.div`
+const NoticeLayout = styled.div`
   display:flex;
   align-items: center;
   justify-content: space-between;
@@ -98,6 +99,98 @@ const NoticeButton = styled.button`
     background-color: #0056b3;
   }
 `
+
+const MainBigContent = styled.div`
+
+margin: 0px 0px 16px;
+font-size: 46px;
+font-weight: bold;
+line-height: 1.4;
+color: rgb(51, 61, 75);
+margin-top:70px;
+`
+const MainSmallContent = styled.div`
+margin: 0px 0px 70px;
+font-size: 18px;
+line-height: 29px;
+color: rgb(107, 118, 132);
+white-space: pre-line;
+
+`
+interface MainIntroLayoutProps {
+  showHiddenContent: boolean;
+  imageUrl:string;
+}
+const MainIntroLayout = styled.div<MainIntroLayoutProps>`
+  
+margin-left: 40px;
+  position: relative;
+  width: 40%;
+  height: 250px;
+  padding: 32px;
+  overflow: hidden;
+  border-radius: 12px;
+  background-image: ${({ imageUrl }) =>
+  `url(${imageUrl})`}; // Replace with your background image URL
+  background-size: cover;
+  background-position: center;
+  transition: transform 0.3s ease;
+  
+  /* Apply blur based on showHiddenContent only to the background image */
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-image: ${({ imageUrl }) =>
+    `url(${imageUrl})`}; // Replace with your background image URL
+    background-size: cover;
+    background-position: center;
+    filter: ${({ showHiddenContent }) => (showHiddenContent ? 'none' : 'blur(5px)')};
+    transition: filter 0.3s ease;
+    z-index: -1; /* Make sure this is rendered below the content */
+  }
+
+  &:hover {
+    transform: scale(1.05);
+  }
+
+  margin-bottom: 30px;
+`;
+
+
+
+const MainIntroContent = styled.div`
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  
+`;
+
+const MainIntroTitle = styled.h1`
+  font-size: 36px;
+  color: white;
+  filter:none;
+`;
+
+const MainIntroDescription = styled.p`
+  font-size: 18px;
+  color: white;
+`;
+const HiddenIntroLayout = styled.div`
+  padding:30px;
+  background-color:transparent;
+  filter:none;
+`
+const MainIntroRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 30px;
+`;
 
 
 const SliderComponent = () => {
@@ -133,17 +226,113 @@ export default function Home(): JSX.Element {
     // 지정된 경로로 이동
     void router.push(path);
   };
+
+  const [showHiddenContent, setShowHiddenContent] = useState(true);
+  const [showHiddenContent2, setShowHiddenContent2] = useState(true);
+  const [showHiddenContent3, setShowHiddenContent3] = useState(true);
+  const [showHiddenContent4, setShowHiddenContent4] = useState(true);
+  const toggleHiddenContent = () => {
+    setShowHiddenContent(!showHiddenContent);
+  };
+  const toggleHiddenContent2 = () => {
+    setShowHiddenContent2(!showHiddenContent2);
+  };
+  const toggleHiddenContent3 = () => {
+    setShowHiddenContent3(!showHiddenContent3);
+  };
+  const toggleHiddenContent4 = () => {
+    setShowHiddenContent4(!showHiddenContent4);
+  };
   return (
     <>
       <SliderComponent />
 
+      <MainBigContent>모두를 위한, 새로운 방안</MainBigContent>
+      <MainSmallContent>어렵고 멀게만 느껴진 모든것들,<br></br>
+        누구나 쉽게 접하고, 다양한 방안들을 확인 가능하게 하는것이 Solver커뮤니티의 존재 이유입니다.</MainSmallContent>
+      <MainIntroRow>
+        <MainIntroLayout imageUrl="main1.png" showHiddenContent={showHiddenContent} onMouseEnter={toggleHiddenContent} onMouseLeave={toggleHiddenContent}>
+          {showHiddenContent ? <MainIntroContent>
+            <MainIntroTitle>Solver가 바꿀 커뮤니티</MainIntroTitle>
+            <MainIntroDescription>
+              편리한 커뮤니티 서비스를 경험하세요.
+            </MainIntroDescription>
+          </MainIntroContent> :
+            <HiddenIntroLayout>
+              <MainIntroContent>
+                <MainIntroTitle>Solver가 바꿀 커뮤니티</MainIntroTitle>
+                <MainIntroDescription>
+                  편리한 커뮤니티 서비스를 경험하세요.
+                </MainIntroDescription>
+              </MainIntroContent>
+            </HiddenIntroLayout>
+          }
+        </MainIntroLayout>
+
+        <MainIntroLayout imageUrl="main2.png" showHiddenContent={showHiddenContent2} onMouseEnter={toggleHiddenContent2} onMouseLeave={toggleHiddenContent2}>
+          {showHiddenContent2 ? <MainIntroContent>
+            <MainIntroTitle>커뮤니티하면 첫번째로,<br/> 생각나는 서비스 Solver</MainIntroTitle>
+            <MainIntroDescription>
+              편리한 커뮤니티 서비스를 경험하세요.
+            </MainIntroDescription>
+          </MainIntroContent> :
+            <HiddenIntroLayout>
+              <MainIntroContent>
+                <MainIntroTitle>커뮤니티하면 첫번째로,<br/> 생각나는 서비스 Solver</MainIntroTitle>
+                <MainIntroDescription>
+                  편리한 커뮤니티 서비스를 경험하세요.
+                </MainIntroDescription>
+              </MainIntroContent>
+            </HiddenIntroLayout>
+          }
+        </MainIntroLayout>
+      </MainIntroRow>
+
+      <MainIntroRow>
+        <MainIntroLayout imageUrl="main3.png" showHiddenContent={showHiddenContent3} onMouseEnter={toggleHiddenContent3} onMouseLeave={toggleHiddenContent3}>
+          {showHiddenContent3 ? <MainIntroContent>
+            <MainIntroTitle>Solver가 바꿀 커뮤니티</MainIntroTitle>
+            <MainIntroDescription>
+              편리한 커뮤니티 서비스를 경험하세요.
+            </MainIntroDescription>
+          </MainIntroContent> :
+            <HiddenIntroLayout>
+              <MainIntroContent>
+                <MainIntroTitle>Solver가 바꿀 커뮤니티</MainIntroTitle>
+                <MainIntroDescription>
+                  편리한 커뮤니티 서비스를 경험하세요.
+                </MainIntroDescription>
+              </MainIntroContent>
+            </HiddenIntroLayout>
+          }
+        </MainIntroLayout>
+
+        <MainIntroLayout imageUrl="main1.png" showHiddenContent={showHiddenContent4} onMouseEnter={toggleHiddenContent4} onMouseLeave={toggleHiddenContent4}>
+          {showHiddenContent4 ? <MainIntroContent>
+            <MainIntroTitle>커뮤니티하면 첫번째로,<br/> 생각나는 서비스 Solver</MainIntroTitle>
+            <MainIntroDescription>
+              편리한 커뮤니티 서비스를 경험하세요.
+            </MainIntroDescription>
+          </MainIntroContent> :
+            <HiddenIntroLayout>
+              <MainIntroContent>
+                <MainIntroTitle>커뮤니티하면 첫번째로,<br/> 생각나는 서비스 Solver</MainIntroTitle>
+                <MainIntroDescription>
+                  편리한 커뮤니티 서비스를 경험하세요.
+                </MainIntroDescription>
+              </MainIntroContent>
+            </HiddenIntroLayout>
+          }
+        </MainIntroLayout>
+      </MainIntroRow>
+
       <SectionContainer>
         <NoticeLayout>
-        <SectionTitle>공지사항</SectionTitle>
+          <SectionTitle>공지사항</SectionTitle>
           <NoticeButton onClick={() => onClickNoticeItem("/Notice")}>+</NoticeButton>
         </NoticeLayout>
-        
-        
+
+
         <ListContainer>
           <ListItem onClick={() => onClickNoticeItem("/Notice")}>
             <ItemTitle >공지사항 1</ItemTitle>
@@ -169,14 +358,14 @@ export default function Home(): JSX.Element {
             <ItemTitle>공지사항 3</ItemTitle>
             <ItemDescription>서비스 이용 안내 및 업데이트 예정 사항입니다.</ItemDescription>
           </ListItem>
-        
+
 
         </ListContainer>
       </SectionContainer>
 
       <SectionContainer>
-      <NoticeLayout>
-        <SectionTitle>자료실</SectionTitle>
+        <NoticeLayout>
+          <SectionTitle>자료실</SectionTitle>
           <NoticeButton onClick={() => onClickNoticeItem("/Resource")}>+</NoticeButton>
         </NoticeLayout>
         <ListContainer>
@@ -196,7 +385,7 @@ export default function Home(): JSX.Element {
             <ItemTitle>자료 1</ItemTitle>
             <ItemDescription>유용한 자료 및 파일을 제공합니다.</ItemDescription>
           </ListItem>
-          <ListItem onClick={() => onClickNoticeItem("/Notice")}> 
+          <ListItem onClick={() => onClickNoticeItem("/Notice")}>
             <ItemTitle>자료 2</ItemTitle>
             <ItemDescription>학습 자료와 참고 문서를 다운로드하세요.</ItemDescription>
           </ListItem>
